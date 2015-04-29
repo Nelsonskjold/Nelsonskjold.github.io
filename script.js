e94adfd9996ea39e533a47db5d6602684a9fbe26
Nelsonskjold <Nelsonskjold@users.noreply.github.com>
2015-04-29 00:47:06 -0400

    redid the calc
    redid the calc, made it easier to figure out the tips. All they we need to count is the total cash. Points and cc tips will be figured out for the user.

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
-function tipsCalc() {
-			var Points = document.getElementById("Points").value
-			var Cash = document.getElementById("Cash").value
-			var CcTips = document.getElementById("CcTips").value
-			
-			var BCash = Cash * 0.05
-			var BCC = CcTips * 0.05
+function pointsCalc() {
+	var servPoint = document.getElementById("numServ").value * 1.0;
+	var runPoint = (document.getElementById("numRun").value) * 0.75;
+	var busPoint = document.getElementById("numBus").value;
 
-			var CashPoint = (Cash - BCash) / Points
-			var CcPoint = (CcTips - BCC) / Points
+	if (busPoint >= 3) {
+		busPoint = busPoint * 0.40;
+		busCut = 0.40;
+	} else {
+		busPoint = busPoint * 0.50;
+		busCut = 0.50;
+	}
 
-			BarCC.innerHTML = BCC.toFixed(2)
-			BarCash.innerHTML = BCash.toFixed(2)
+	var points = servPoint + runPoint + busPoint;
 
-			ServCC.innerHTML = CcPoint.toFixed(2)
-			ServCash.innerHTML = CashPoint.toFixed(2)
+	totalPoints.innerHTML = points.toFixed(2);
+	tipsCalc(points, busCut);
 
-			RunCC.innerHTML = (CcPoint * 0.75).toFixed(2)
-			RunCash.innerHTML = (CashPoint * 0.75).toFixed(2)
+}
 
-			BusCC.innerHTML = (CcPoint * 0.5).toFixed(2)
-			BusCash.innerHTML = (CashPoint * 0.5).toFixed(2)
+function tipsCalc(x, bCut) {
+	var totalCash = document.getElementById("cash").value;
+	var totalCC = 0;
+	var points = x;
+	var busCut = bCut;
 
-			BussersCC.innerHTML = (CcPoint * 0.4).toFixed(2)
-			BussersCash.innerHTML = (CashPoint * 0.4).toFixed(2)
+	for (var i = 0; i <= 5; i++ ) {
+		 totalCC += document.getElementById("ccTips" + i).value * 1.00;
+	}
 
-			RunHalfCC.innerHTML = (CcPoint * 0.38).toFixed(2)
-			RunHalfCash.innerHTML = (CashPoint * 0.38).toFixed(2)
+	var bCash = Math.round(totalCash * 0.05);
+	var bCC = (totalCC * 0.05).toFixed(2);
 
-			BusHalfCC.innerHTML = (CcPoint * 0.25).toFixed(2)
-			BusHalfCash.innerHTML = (CashPoint * 0.25).toFixed(2)
+	var cashPoint = (totalCash - bCash) / points;
+	var ccPoint = (totalCC - bCC) / points;
+	
+	barCash.innerHTML = bCash;
+	barCC.innerHTML = bCC;
 
-		}
+	halfBarCash.innerHTML = Math.round(bCash / 2);
+	halfBarCC.innerHTML = (bCC / 2).toFixed(2);
+
+	servCash.innerHTML = Math.round(cashPoint);
+	servCc.innerHTML = ccPoint.toFixed(2);
+
+	runCash.innerHTML = Math.round(cashPoint * 0.75);
+	runCC.innerHTML = (ccPoint * 0.75).toFixed(2);
+
+	busCash.innerHTML = Math.round(cashPoint * busCut);
+	busCC.innerHTML = (ccPoint * busCut).toFixed(2);
+
+	runHalfCash.innerHTML = Math.round(cashPoint * 0.38);
+	runHalfCC.innerHTML = (ccPoint * 0.38).toFixed(2);
+
+	busHalfCash.innerHTML = Math.round(cashPoint * 0.25);
+	busHalfCC.innerHTML = (ccPoint * 0.25).toFixed(2);
+
+}
